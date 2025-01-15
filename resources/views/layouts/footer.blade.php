ae85b41eda3ebeb8fba1a8952e37c78129684eff
Krishna Mandal <mandalkrishna739@gmail.com>
2025-01-15 08:55:12 +0545

    basic templates and assets setup

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,32 @@
+
+        <!-- Footer Start -->
+        <div id="footer">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <p>&copy; Copyright <a href="http://calmandcode.com">CalmAndCode</a>. All Rights Reserved</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>Template by <a href="">CalmAndCode</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        
+        <!-- Back to Top -->
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+        
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('assets/lib/easing/easing.min.js') }}"></script>
+        <script src="{{ asset('assets/lib/menuspy/menuspy.min.js') }}"></script>
+        <script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+        <!-- Template Javascript -->
+        <script src="{{ asset('assets/js/main.js') }}"></script>
+    </body>
+</html>
