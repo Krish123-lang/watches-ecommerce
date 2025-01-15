b3b5acf9ac61ac4540742f996f0ede6a8b90c2b4
Krishna Mandal <mandalkrishna739@gmail.com>
2025-01-15 17:23:32 +0545

    Add to cart logic updated and getting total price on the checkout page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,11 +6,142 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function cart(){
+    public function cart()
+    {
         return view('cart');
     }
-    
-    public function add_to_cart(Request $request){
 
+    // public function add_to_cart(Request $request)
+    // {
+    //     if ($request->session()->has('cart')) {
+    //         $cart = $request->session()->get('cart');
+    //         $product_array_ids = array_column($cart, 'id');
+    //         $id = $request->input('id');
+
+    //         if (!in_array($id, $product_array_ids)) {
+    //             $name = $request->input('name');
+    //             $image = $request->input('image');
+    //             $price = $request->input('price');
+    //             $quantity = $request->input('quantity');
+    //             $sale_price = $request->input('sale_price');
+
+    //             if ($sale_price != null) {
+    //                 $price_to_charge = $sale_price;
+    //             } else {
+    //                 $price_to_charge = $price;
+    //             }
+
+    //             $product_array = array(
+    //                 'id' => $id,
+    //                 'name' => $name,
+    //                 'image' => $image,
+    //                 'price' => $price_to_charge,
+    //                 'quantity' => $quantity,
+    //             );
+    //             $cart[$id] = $product_array;
+    //             $request->session()->put('cart', $cart);
+    //         } else {
+    //             echo "<script>alert('Product is already in the cart!');</script>";
+    //         }
+    //         return view('cart');
+    //     } else {
+    //         $cart = array();
+    //         $name = $request->input('name');
+    //         $image = $request->input('image');
+    //         $price = $request->input('price');
+    //         $quantity = $request->input('quantity');
+    //         $sale_price = $request->input('sale_price');
+
+    //         if ($sale_price != null) {
+    //             $price_to_charge = $sale_price;
+    //         } else {
+    //             $price_to_charge = $price;
+    //         }
+
+    //         $product_array = array(
+    //             'id' => $id,
+    //             'name' => $name,
+    //             'image' => $image,
+    //             'price' => $price_to_charge,
+    //             'quantity' => $quantity,
+    //         );
+    //         $cart[$id] = $product_array;
+    //         $request->session()->put('cart', $cart);
+    //         return view('cart');
+    //     }
+    // }
+
+    public function add_to_cart(Request $request)
+    {
+        $id = $request->input('id'); 
+
+        if ($request->session()->has('cart')) {
+            $cart = $request->session()->get('cart');
+            $product_array_ids = array_column($cart, 'id');
+
+            if (!in_array($id, $product_array_ids)) {
+                $name = $request->input('name');
+                $image = $request->input('image');
+                $price = $request->input('price');
+                $quantity = $request->input('quantity');
+                $sale_price = $request->input('sale_price');
+
+                $price_to_charge = $sale_price != null ? $sale_price : $price;
+
+                $product_array = array(
+                    'id' => $id,
+                    'name' => $name,
+                    'image' => $image,
+                    'price' => $price_to_charge,
+                    'quantity' => $quantity,
+                );
+
+                $cart[$id] = $product_array;
+                $request->session()->put('cart', $cart);
+            } else {
+                echo "<script>alert('Product is already in the cart!');</script>";
+            }
+            $this->calculateTotalCart($request);
+            return view('cart');
+        } else {
+            $cart = array();
+            $name = $request->input('name');
+            $image = $request->input('image');
+            $price = $request->input('price');
+            $quantity = $request->input('quantity');
+            $sale_price = $request->input('sale_price');
+
+            $price_to_charge = $sale_price != null ? $sale_price : $price;
+
+            $product_array = array(
+                'id' => $id,
+                'name' => $name,
+                'image' => $image,
+                'price' => $price_to_charge,
+                'quantity' => $quantity,
+            );
+
+            $cart[$id] = $product_array;
+            $request->session()->put('cart', $cart);
+            $this->calculateTotalCart($request);
+            return view('cart');
+        }
+    }
+
+    public function calculateTotalCart(Request $request)
+    {
+        $cart = $request->session()->get('cart');
+        $total_price = 0;
+        $total_quantity = 0;
+
+        foreach ($cart as $id => $product) {
+            $product = $cart[$id];
+            $price = $product['price'];
+            $quantity = $product['quantity'];
+            $total_price = $total_price + ($price * $quantity);
+            $total_quantity = $total_quantity + $quantity;
+        }
+        $request->session()->put('total', $total_price);
+        $request->session()->put('quantity', $total_quantity);
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -20,51 +20,56 @@
                         <th>Subtotal</th>
                     </tr>
 
-                    <tr>
-                        <td>
-                            <div class="product-info">
-                                <img src="img/menu-1.jpg">
-                                <div>
-                                    <p>Hot Coffee</p>
-                                    <small><span>$</span>199</small>
-                                    <br>
+                    @if (Session::has('cart'))
+                        @foreach (Session::get('cart') as $product)
+                            <tr>
+                                <td>
+                                    <div class="product-info">
+                                        <img src="{{ asset('assets/img/'.$product['image']) }}">
+                                        <div>
+                                            <p>{{$product['name']}}</p>
+                                            <small><span>$</span>{{$product['price']}}</small>
+                                            <br>
+                                            <form>
+
+                                                <input type="submit" name="remove_btn" class="remove-btn" value="remove">
+                                            </form>
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td>
                                     <form>
-
-                                        <input type="submit" name="remove_btn" class="remove-btn" value="remove">
+                                        <input type="number" name="quantity" value="{{$product['quantity']}}">
+                                        <input type="submit" value="edit" class="edit-btn" name="edit_product_quantity_btn">
                                     </form>
-                                </div>
-                            </div>
-                        </td>
+                                </td>
 
-                        <td>
-                            <form>
-                                <input type="number" name="quantity" value="1">
-                                <input type="submit" value="edit" class="edit-btn" name="edit_product_quantity_btn">
-                            </form>
-                        </td>
-
-                        <td>
-                            <span class="product-price">$199</span>
-                        </td>
-                    </tr>
+                                <td>
+                                    <span class="product-price">${{ $product['price'] * $product['quantity'] }}</span>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
                 </table>
 
                 <div class="cart-total">
                     <table>
-                        <tr>
-                            <td>Total</td>
-                            <td>$199</td>
-                        </tr>
-
+                        @if (Session::has('cart'))
+                            <tr>
+                                <td>Total</td>
+                                @if (Session::has('total'))
+                                    <td>${{Session::get('total')}}</td>
+                                @endif
+                            </tr>
+                        @endif
                     </table>
                 </div>
 
                 <div class="checkout-container">
-
                     <form>
                         <input type="submit" class="btn checkout-btn" value="Checkout" name="">
                     </form>
-
                 </div>
 
             </section>
